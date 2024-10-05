068b215ee68da27438d483cb62b8c2fa92d4c875
Gabri7 <66312272+Gabri7@users.noreply.github.com>
2024-10-05 18:00:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const startButton = document.querySelector('.start-button');
+    
+    startButton.addEventListener('click', function() {
+        window.location.href = 'proximaB.html';
+    });
+});
